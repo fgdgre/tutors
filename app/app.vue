e1c26939882446b5094c186e668420c4e792a8d7
fgdgre <ddemin20052016@gmail.com>
2025-12-09 01:39:57 +0200

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="test">
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<style lang="scss">
+.test {
+  display: flex;
+  width: 100%;
+}
+</style>
