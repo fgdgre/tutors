3925ed9bdebbdc446f883af94dd8e07b4239321d
fgdgre <ddemin20052016@gmail.com>
2025-12-09 14:11:51 +0200

    implement header for mobile dimension

---
+++ app/pages/tutors.vue
@@ -0,0 +1,11 @@
+<template>tutors</template>
+
+<style lang="scss">
+@use "@/assets/scss/variables" as *;
+
+.test {
+  display: flex;
+  width: 100%;
+  background-color: $color-primary;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   ssr: false,
+  appDir: "/app",
+  css: ["/assets/scss/main.scss"],
 });
